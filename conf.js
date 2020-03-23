13133c8a4cebe9a2a59625aca57cb22c45db7e67
Mohd Jeeshan <mohdjeeshan007@gmail.com>
2020-03-23 18:45:01 +0530

    Added the chrome headless option in conf.js

--- conf.js
+++ conf.js
@@ -12,7 +12,7 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     chromeOptions: {
-      args: ['--disable-notifications']
+      args: ['--disable-notifications', '--headless', '--no-sandbox']
     }
   },
   onPrepare: () => {
